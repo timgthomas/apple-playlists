939b9b6943c03ed88a6af86e1a4783e5501584bc
Tim G. Thomas <tim@timgthomas.com>
2021-11-26 15:53:25 -0600

    switch to module format

--- app.js
+++ app.js
@@ -1,45 +1,47 @@
-document.addEventListener('alpine:init', () => {
-  Alpine.store('app', {
+import Alpine from 'alpinejs'
 
-    filter: '',
+Alpine.store('app', {
 
-    get favoriteIds() {
-      return (localStorage.getItem('favorites')?.split(',') ?? []).filter(Boolean)
-    },
-    set favoriteIds(newFavoriteIds) {
-      localStorage.setItem('favorites', newFavoriteIds.filter(Boolean).join(','));
-    },
+  filter: '',
 
-    get filteredPlaylists() {
-      let filter = this.filter.toLowerCase().trim()
-      return window.playlistData
-        .filter((p) => !filter || p.slug.replace(/-/g, ' ').includes(filter))
-        .map((p) => ({ ...p, isFavorite: this.favoriteIds.includes(p.id) }))
-    },
-    get groups() {
-      let groupedPlaylists = Object
-        .entries(_.groupBy(this.filteredPlaylists, 'group'))
-        .map(([ name, playlists ]) => ({ name, playlists }))
-      return [
-        {
-          name: 'Favorites',
-          playlists: this.filteredPlaylists.filter((p) => p.isFavorite),
-        },
-        ...groupedPlaylists,
-      ].filter((g) => g.playlists.length)
-    },
+  get favoriteIds() {
+    return (localStorage.getItem('favorites')?.split(',') ?? []).filter(Boolean)
+  },
+  set favoriteIds(newFavoriteIds) {
+    localStorage.setItem('favorites', newFavoriteIds.filter(Boolean).join(','));
+  },
 
-    buildHref({ id, slug }) {
-      return `https://music.apple.com/us/playlist/${slug}/pl.${id}`
-    },
+  get filteredPlaylists() {
+    let filter = this.filter.toLowerCase().trim()
+    return window.playlistData
+      .filter((p) => !filter || p.slug.replace(/-/g, ' ').includes(filter))
+      .map((p) => ({ ...p, isFavorite: this.favoriteIds.includes(p.id) }))
+  },
+  get groups() {
+    let groupedPlaylists = Object
+      .entries(_.groupBy(this.filteredPlaylists, 'group'))
+      .map(([ name, playlists ]) => ({ name, playlists }))
+    return [
+      {
+        name: 'Favorites',
+        playlists: this.filteredPlaylists.filter((p) => p.isFavorite),
+      },
+      ...groupedPlaylists,
+    ].filter((g) => g.playlists.length)
+  },
 
-    toggleFavorite({ id }) {
-      if (this.favoriteIds.includes(id)) {
-        this.favoriteIds = this.favoriteIds.filter((favoriteId) => favoriteId !== id)
-      } else {
-        this.favoriteIds = [ ...this.favoriteIds, id ]
-      }
-    },
+  buildHref({ id, slug }) {
+    return `https://music.apple.com/us/playlist/${slug}/pl.${id}`
+  },
 
-  })
+  toggleFavorite({ id }) {
+    if (this.favoriteIds.includes(id)) {
+      this.favoriteIds = this.favoriteIds.filter((favoriteId) => favoriteId !== id)
+    } else {
+      this.favoriteIds = [ ...this.favoriteIds, id ]
+    }
+  },
+
 })
+
+Alpine.start()
